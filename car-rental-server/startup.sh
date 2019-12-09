70350bfa180664395644f1519717ad4531bec827
Chi Hoang <chhoang102@gmail.com>
2019-12-08 18:44:32 -0800

    remove command

--- car-rental-server/startup.sh
+++ car-rental-server/startup.sh
@@ -10,6 +10,6 @@ kill -9 $(lsof -i:8443 -t) 2> /dev/null
 echo "Killed port 8443"
 mvn clean install
 
-java -jar target/car-rental-server-0.0.1-SNAPSHOT.jar > ./carrentalserver-${DATE_WITH_TIME}.log
+# java -jar target/car-rental-server-0.0.1-SNAPSHOT.jar > ./carrentalserver-${DATE_WITH_TIME}.log
 
 echo "Started carrentalserver application"
