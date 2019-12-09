20729f988ae591ed5f6c895590ba20f89f418606
Chi Hoang <chhoang102@gmail.com>
2019-12-08 17:20:38 -0800

    startup script

---
+++ car-rental-server/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Remove old logs"
+rm -rf ./carrentalserver*
+
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+echo $DATE_WITH_TIME
+
+kill -9 $(lsof -i:8443 -t) 2> /dev/null
+echo "Killed port 8443"
+mvn clean install
+
+nohup java -jar target/car-rental-server-0.0.1-SNAPSHOT.jar > ./carrentalserver-${DATE_WITH_TIME}.log &
+
+echo "Started carrentalserver application"
